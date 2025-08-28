71f9ba23ec778002d9af8909a08271d14951e5b2
Pedro Jaber <pedrojaberc@gmail.com>
2025-08-28 00:09:54 -0300

    27/08 part 2

--- seção-24-cadastro-de-usuario/crud/frontend/src/main/App.jsx
+++ seção-24-cadastro-de-usuario/crud/frontend/src/main/App.jsx
@@ -1,4 +1,16 @@
 import React from "react";
 import "./App.css";
 
-import Header from "../components/template/Header";
+import Logo from "../components/template/Logo";
+import Nav from "../components/template/Nav";
+import Main from "../components/template/Main";
+import Footer from "../components/template/Footer";
+
+export default (props) => (
+  <div className="app">
+    <Logo />
+    <Nav />
+    <Main />
+    <Footer />
+  </div>
+);
