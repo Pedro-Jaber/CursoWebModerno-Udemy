a1beeae97bff2ea8d52cd7b7584a8c4a97a96f5a
Pedro Jaber <pedrojaberc@gmail.com>
2025-09-05 22:49:04 -0300

    05/09

---
+++ seção-28-calculadora-Vue/calculator/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <h1>Calculator</h1>
+    <CalculatorVue />
+  </div>
+</template>
+
+<script>
+import CalculatorVue from "./main/CalculatorVue.vue";
+
+export default {
+  components: { CalculatorVue },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "RobotoMono";
+  src: url("./fonts/RobotoMono-Thin.ttf");
+}
+
+* {
+  font-family: "RobotoMono", monospace;
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #fff;
+  background-color: hsl(204, 5%, 15%);
+}
+</style>
---
+++ seção-28-calculadora-Vue/calculator/src/main/CalculatorVue.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="calculator">
+    <DisplayVue></DisplayVue>
+    <ButtonVue></ButtonVue>
+  </div>
+</template>
+
+<script>
+import DisplayVue from "../components/DisplayVue.vue";
+import ButtonVue from "../components/ButtonVue.vue";
+
+export default {
+  components: { ButtonVue, DisplayVue },
+};
+</script>
+
+<style>
+.calculator {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
+
+  background-color: hsl(26, 20%, 17%);
+
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr repeat(5, 3rem);
+}
+</style>
